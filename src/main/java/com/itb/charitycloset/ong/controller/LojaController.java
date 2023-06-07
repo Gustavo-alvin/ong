73e153710170c9b76fc7ed9202a49f52d8c2c579
Gustavo alves <gustavoboygame@gmail.com>
2023-06-07 07:50:22 -0300

    inicio

---
+++ src/main/java/com/itb/charitycloset/ong/controller/LojaController.java
@@ -0,0 +1,20 @@
+package com.itb.charitycloset.ong.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/ong/produto")
+public class LojaController {
+	
+	@GetMapping("/listar")
+	
+	public String listarProduto() {
+		
+		return "produtos";
+	}
+	
+	
+
+}
---
+++ src/main/java/com/itb/charitycloset/ong/model/Produto.java
@@ -0,0 +1,16 @@
+package com.itb.charitycloset.ong.model;
+
+public class Produto {
+	
+	private long id;
+	private String nome;
+	private String descricao;
+	private String codigoBarras;
+	private double preco;
+	
+	// public: acesso liberado para todas as classes.
+	
+	// private: acesso permitido apenas para os membros
+	// da propria classe
+	// entende-se por membros(atributos e ou metodos)
+}
